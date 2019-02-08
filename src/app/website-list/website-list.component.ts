7aa48913e98a836525d6128b9fbb9e1903d3f2c7
Simon Reitinger <s.reitinger@agentur-dreibein.de>
2019-02-08 14:50:14 +0100

    website list to material components

--- src/app/website-list/website-list.component.ts
+++ src/app/website-list/website-list.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { WebsiteService } from '../services/website.service';
 import { WebsiteModel } from '../models/website.model';
 import { ContaoManagerService } from '../services/contao-manager.service';
 import { SoftwareService } from '../services/software.service';
 import { SoftwareModel } from '../models/software.model';
 import { WebsiteAddComponent } from '../website-add/website-add.component';
-import { MatDialog, MatTableDataSource } from '@angular/material';
+import { MatDialog, MatSort, MatTableDataSource } from '@angular/material';
+import { MonitoringComponent } from '../monitoring/monitoring.component';
+
+const TABLE_OPTIONS = {
+  displayedColumns: ['cleanUrl', 'software', 'softwareVersion', 'platform', 'platformVersion', 'status', 'detail']
+};
 
 @Component({
   selector: 'app-website-list',
@@ -14,9 +19,13 @@ import { MatDialog, MatTableDataSource } from '@angular/material';
 })
 export class WebsiteListComponent implements OnInit {
 
+  tableOptions = TABLE_OPTIONS;
+
   websites: WebsiteModel[];
   softwares: SoftwareModel[];
   phpVersions: string[];
+
+  @ViewChild(MatSort) sort: MatSort;
   dataSource: MatTableDataSource<WebsiteModel>;
 
   constructor(
@@ -30,6 +39,7 @@ export class WebsiteListComponent implements OnInit {
   ngOnInit() {
     this.ws.getAll().subscribe((res: WebsiteModel[]) => {
       this.websites = res;
+      console.log(res);
       this.dataSource = new MatTableDataSource(this.websites);
     });
     this.ss.getAll().subscribe((res: SoftwareModel[]) => {
@@ -40,6 +50,29 @@ export class WebsiteListComponent implements OnInit {
           break;
         }
       }
+    });
+  }
+
+  sortData(event) {
+    this.dataSource.data = this.websites.sort((a, b) => {
+      switch (event.direction) {
+        case 'asc':
+        default:
+          return a[event.active] > b[event.active] ? 1 : -1;
+        case 'desc':
+          return a[event.active] < b[event.active] ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+
+  openMonitoringDialog(website: WebsiteModel) {
+    this.dialog.open(MonitoringComponent, {
+      data: {
+        website: website
+      },
+      height: '450px',
+      width: '800px'
     });
   }
 
